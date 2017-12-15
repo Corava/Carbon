fdb356b8861566f1033982a3fae0ae500a8c6c69
TODOROV <9551957@graniteschools.org>
2017-12-15 11:31:49 -0700

    Baseline

---
+++ src/Carbon Library/Executable Carbon Test/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace Executable_Carbon_Test
+{
+    public class Class1 {
+        public static void Main() {
+            Debug.WriteLine("Hello");
+        }
+    }
+}
